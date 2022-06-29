500c53f69b04f948a047dd7bdf636fda8a532e76
Jack Kellenberger <jackson@kellenberger.io>
2022-06-29 00:32:17 -0400

    Implement arithmetic functions and operate()

---
+++ calculator.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    if (b === 0) return '#DIV/0!';
+    return a/b;
+}
+
+function operate(operand1, operator, operand2) {
+    switch(operator) {
+        case '+':
+            return add(operand1, operand2);
+        case '-':
+            return subtract(operand1, operand2);
+        case '*':
+            return multiply(operand1, operand2);
+        case '/':
+            return divide(operand1, operand2);
+    }
+}
